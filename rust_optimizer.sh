bd87f2ad18f8ccef7bf77608b068e27f934a959c
Sai Kumar <sai@umee.cc>
2022-12-08 18:13:57 +0530

    chore: bump the cm-umee-types version

--- rust_optimizer.sh
+++ rust_optimizer.sh
@@ -4,11 +4,18 @@ set -eo pipefail
 
 CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BASENAME="$(basename $CWD)"
-OPTIMIZER_VERSION="0.12.6"
+OPTIMIZER_VERSION="0.12.8"
+architecture=$(uname -m)
 
-docker run --rm -v $CWD:/code \
-  --mount type=volume,source="${BASENAME}_cache",target=/code/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/rust-optimizer:$OPTIMIZER_VERSION
+if [[ "$architecture" == "arm64" ]];then
+  docker run --rm -v $CWD:/code \
+    --mount type=volume,source="${BASENAME}_cache",target=/code/target \
+    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+    cosmwasm/rust-optimizer-arm64:$OPTIMIZER_VERSION
+else
+  docker run --rm -v $CWD:/code \
+    --mount type=volume,source="${BASENAME}_cache",target=/code/target \
+    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+    cosmwasm/rust-optimizer:$OPTIMIZER_VERSION
 
 echo Finish rust optimizer
